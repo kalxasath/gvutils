49009cc73633a005cbc206e5d942589ce07e1c0e
George Vrynios <gvrynios@gmail.com>
2017-08-11 16:21:58 +0300

    creation DATE added
    creation DATE added

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports = {
   screen: {
     /**
      * sends a clear screen sequence to the tty
-     *
+     * GV 11/08/2017
      */
     cls: () => {process.stdout.write('\u001Bc')}
   }
